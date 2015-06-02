581ac84f4ca4fc40cc09bf638b76a3087f05c57f
Hannes Dorfmann <hannes.dorfmann@gmail.com>
2015-06-02 15:57:54 +0200

    Replaced JavaWriter with JavaPoet

--- factory/processor/src/main/java/com/hannesdorfmann/annotationprocessing101/factory/processor/FactoryGroupedClasses.java
+++ factory/processor/src/main/java/com/hannesdorfmann/annotationprocessing101/factory/processor/FactoryGroupedClasses.java
@@ -17,10 +17,11 @@
 package com.hannesdorfmann.annotationprocessing101.factory.processor;
 
 import com.hannesdorfmann.annotationprocessing101.factory.annotation.Factory;
-import com.squareup.javawriter.JavaWriter;
+import com.squareup.javapoet.JavaFile;
+import com.squareup.javapoet.MethodSpec;
+import com.squareup.javapoet.TypeName;
+import com.squareup.javapoet.TypeSpec;
 import java.io.IOException;
-import java.io.Writer;
-import java.util.EnumSet;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import javax.annotation.processing.Filer;
@@ -28,7 +29,6 @@ import javax.lang.model.element.Modifier;
 import javax.lang.model.element.PackageElement;
 import javax.lang.model.element.TypeElement;
 import javax.lang.model.util.Elements;
-import javax.tools.JavaFileObject;
 
 /**
  * This class holds all {@link FactoryAnnotatedClass}s that belongs to one factory. In other words,
@@ -74,49 +74,84 @@ public class FactoryGroupedClasses {
     itemsMap.put(toInsert.getId(), toInsert);
   }
 
+  public void generateCode(Elements elementUtils, Filer filer) throws IOException {
+    TypeElement superClassName = elementUtils.getTypeElement(qualifiedClassName);
+    String factoryClassName = superClassName.getSimpleName() + SUFFIX;
+    String qualifiedFactoryClassName = qualifiedClassName + SUFFIX;
+    PackageElement pkg = elementUtils.getPackageOf(superClassName);
+    String packageName = pkg.isUnnamed() ? null : pkg.getQualifiedName().toString();
+
+    MethodSpec.Builder method = MethodSpec.methodBuilder("create")
+        .addModifiers(Modifier.PUBLIC)
+        .addParameter(String.class, "id")
+        .returns(TypeName.get(superClassName.asType()));
+
+    // check if id is null
+    method.beginControlFlow("if (id == null)")
+        .addStatement("throw new IllegalArgumentException($S)", "id is null!")
+        .endControlFlow();
+
+    // Generate items map
+
+    for (FactoryAnnotatedClass item : itemsMap.values()) {
+      method.beginControlFlow("if ($S.equals(id))", item.getId())
+          .addStatement("return new $L()", item.getTypeElement().getQualifiedName().toString())
+          .endControlFlow();
+    }
+
+    method.addStatement("throw new IllegalArgumentException($S + id)", "Unknown id = ");
+
+    TypeSpec typeSpec = TypeSpec.classBuilder(factoryClassName).addMethod(method.build()).build();
+
+    // Write file
+    JavaFile.builder(packageName, typeSpec).build().writeTo(filer);
+  }
+
   /**
    * Generate the java code
    *
    * @throws IOException
-   */
+
   public void generateCode(Elements elementUtils, Filer filer) throws IOException {
 
-    TypeElement superClassName = elementUtils.getTypeElement(qualifiedClassName);
-    String factoryClassName = superClassName.getSimpleName() + SUFFIX;
+  TypeElement superClassName = elementUtils.getTypeElement(qualifiedClassName);
+  String factoryClassName = superClassName.getSimpleName() + SUFFIX;
 
-    JavaFileObject jfo = filer.createSourceFile(qualifiedClassName + SUFFIX);
-    Writer writer = jfo.openWriter();
-    JavaWriter jw = new JavaWriter(writer);
+  JavaFileObject jfo = filer.createSourceFile(qualifiedClassName + SUFFIX);
+  Writer writer = jfo.openWriter();
+  JavaWriter jw = new JavaWriter(writer);
 
-    // Write package
-    PackageElement pkg = elementUtils.getPackageOf(superClassName);
-    if (!pkg.isUnnamed()) {
-      jw.emitPackage(pkg.getQualifiedName().toString());
-      jw.emitEmptyLine();
-    } else {
-      jw.emitPackage("");
-    }
-
-    jw.beginType(factoryClassName, "class", EnumSet.of(Modifier.PUBLIC));
-    jw.emitEmptyLine();
-    jw.beginMethod(qualifiedClassName, "create", EnumSet.of(Modifier.PUBLIC), "String", "id");
-
-    jw.beginControlFlow("if (id == null)");
-    jw.emitStatement("throw new IllegalArgumentException(\"id is null!\")");
-    jw.endControlFlow();
-
-    for (FactoryAnnotatedClass item : itemsMap.values()) {
-      jw.beginControlFlow("if (\"%s\".equals(id))", item.getId());
-      jw.emitStatement("return new %s()", item.getTypeElement().getQualifiedName().toString());
-      jw.endControlFlow();
-      jw.emitEmptyLine();
-    }
-
-    jw.emitStatement("throw new IllegalArgumentException(\"Unknown id = \" + id)");
-    jw.endMethod();
-
-    jw.endType();
-
-    jw.close();
+  // Write package
+  PackageElement pkg = elementUtils.getPackageOf(superClassName);
+  if (!pkg.isUnnamed()) {
+  jw.emitPackage(pkg.getQualifiedName().toString());
+  jw.emitEmptyLine();
+  } else {
+  jw.emitPackage("");
   }
+
+  jw.beginType(factoryClassName, "class", EnumSet.of(Modifier.PUBLIC));
+  jw.emitEmptyLine();
+  jw.beginMethod(qualifiedClassName, "create", EnumSet.of(Modifier.PUBLIC), "String", "id");
+
+  jw.beginControlFlow("if (id == null)");
+  jw.emitStatement("throw new IllegalArgumentException(\"id is null!\")");
+  jw.endControlFlow();
+
+  for (FactoryAnnotatedClass item : itemsMap.values()) {
+  jw.beginControlFlow("if (\"%s\".equals(id))", item.getId());
+  jw.emitStatement("return new %s()", item.getTypeElement().getQualifiedName().toString());
+  jw.endControlFlow();
+  jw.emitEmptyLine();
+  }
+
+  jw.emitStatement("throw new IllegalArgumentException(\"Unknown id = \" + id)");
+  jw.endMethod();
+
+  jw.endType();
+
+  jw.close();
+  }
+
+   */
 }
